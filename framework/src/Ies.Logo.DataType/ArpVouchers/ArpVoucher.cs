9e1379a03fcc74b0e8d99c2c7557d2e9943eb6d8
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-18 12:48:11 +0300

    ArpVoucher domaini tamamlandı

--- framework/src/Ies.Logo.DataType/ArpVouchers/ArpVoucher.cs
+++ framework/src/Ies.Logo.DataType/ArpVouchers/ArpVoucher.cs
@@ -1,18 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Ies.Logo.DataType.AggregateRoot;
 using Ies.Logo.DataType.DefinitionFields;
+using Ies.Logo.DataType.Infrastructure;
 
 namespace Ies.Logo.DataType.ArpVouchers
 {
     [Serializable]
-    public class ArpVoucher : AuditedAggregateRoot
+    public class ArpVoucher : AuditedAggregateRoot, IAuxilCode, IAuthCode, IDivision, IDepartment, INotes, ICancelled, IDataSiteId, IProjectCode, IApprove
     {
-
+        public virtual string Number { get; set; }
+        public virtual Nullable<DateTime> Date { get; set; }
+        public virtual string DocNumber { get; set; }
+        public virtual Nullable<int> Type { get; set; }
+        public virtual Nullable<decimal> TotalDebit { get; set; }
+        public virtual Nullable<decimal> TotalCredit { get; set; }
+        public virtual Nullable<decimal> RcTotalDebit { get; set; }
+        public virtual Nullable<decimal> RcTotalCredit { get; set; }
+        public virtual Nullable<short> GlPosted { get; set; }
+        public virtual string GlCode { get; set; }
+        public virtual string OhpCode { get; set; }
+        public virtual Nullable<int> PrintCounter { get; set; }
+        public virtual Nullable<int> Accficheref { get; set; }
+        public virtual Nullable<int> CurrselTotals { get; set; }
+        public virtual Nullable<int> CurrselDetails { get; set; }
+        public virtual string ArpCode { get; set; }
+        public virtual Nullable<int> Time { get; set; }
+        public virtual string BankaccCode { get; set; }
+        public virtual string BnglCode { get; set; }
+        public virtual string BnohpCode { get; set; }
+        public virtual Nullable<short> AffectCollatrl { get; set; }
+        public virtual Nullable<int> Status { get; set; }
+        public virtual Nullable<short> Grpfirmtrans { get; set; }
+        public virtual Nullable<short> AffectRisk { get; set; }
+        public virtual string PosTerminalNr { get; set; }
+        public virtual Nullable<int> AccFicheSiteid { get; set; }
+        public virtual Nullable<DateTime> DocDate { get; set; }
+        public virtual string SalesmanCode { get; set; }
+        public virtual Nullable<short> CancelAutoGlProc { get; set; }
+        public virtual Nullable<DateTime> EbookDocdate { get; set; }
 
         #region Implementation
-
+        public virtual string AuthCode { get; set; }
+        public virtual string AuxilCode { get; set; }
+        public virtual Nullable<short> Division { get; set; }
+        public virtual Nullable<short> Department { get; set; }
+        public virtual string Notes1 { get; set; }
+        public virtual string Notes2 { get; set; }
+        public virtual string Notes3 { get; set; }
+        public virtual string Notes4 { get; set; }
+        public virtual Nullable<short> Cancelled { get; set; }
+        public virtual Nullable<int> DataSiteId { get; set; }
+        public virtual string ProjectCode { get; set; }
+        public virtual Nullable<short> Approve { get; set; }
+        public virtual Nullable<DateTime> ApproveDate { get; set; }
         #endregion
 
         #region SubClasses
--- framework/src/Ies.Logo.DataType/ArpVouchers/ArpVoucherProfile.cs
+++ framework/src/Ies.Logo.DataType/ArpVouchers/ArpVoucherProfile.cs
@@ -25,9 +25,39 @@ namespace Ies.Logo.DataType.ArpVouchers
             typeConfigurations.Name<T>("ARP_VOUCHER");
 
             typeConfigurations
-                 .Member(m => m.DefnFldsList).Name("DEFNFLDSLIST")
-                 .Member(m => m.Transactions).Name("TRANSACTIONS")
-                 ;
+                .Member(m => m.Number).Name("NUMBER ")
+                .Member(m => m.Date).Name("DATE")
+                .Member(m => m.DocNumber).Name("DOC_NUMBER ")
+                .Member(m => m.Type).Name("TYPE ")
+                .Member(m => m.TotalDebit).Name("TOTAL_DEBIT ")
+                .Member(m => m.TotalCredit).Name("TOTAL_CREDIT ")
+                .Member(m => m.RcTotalDebit).Name("RC_TOTAL_DEBIT ")
+                .Member(m => m.RcTotalCredit).Name("RC_TOTAL_CREDIT ")
+                .Member(m => m.GlPosted).Name("GL_POSTED ")
+                .Member(m => m.GlCode).Name("GL_CODE ")
+                .Member(m => m.OhpCode).Name("OHP_CODE ")
+                .Member(m => m.PrintCounter).Name("PRINT_COUNTER ")
+                .Member(m => m.Accficheref).Name("ACCFICHEREF ")
+                .Member(m => m.CurrselTotals).Name("CURRSEL_TOTALS ")
+                .Member(m => m.CurrselDetails).Name("CURRSEL_DETAILS ")
+                .Member(m => m.ArpCode).Name("ARP_CODE ")
+                .Member(m => m.Time).Name("TIME ")
+                .Member(m => m.BankaccCode).Name("BANKACC_CODE ")
+                .Member(m => m.BnglCode).Name("BNGL_CODE ")
+                .Member(m => m.BnohpCode).Name("BNOHP_CODE ")
+                .Member(m => m.AffectCollatrl).Name("AFFECT_COLLATRL ")
+                .Member(m => m.Status).Name("STATUS ")
+                .Member(m => m.Grpfirmtrans).Name("GRPFIRMTRANS ")
+                .Member(m => m.AffectRisk).Name("AFFECT_RISK ")
+                .Member(m => m.PosTerminalNr).Name("POS_TERMINAL_NR ")
+                .Member(m => m.AccFicheSiteid).Name("ACC_FICHE_SITEID ")
+                .Member(m => m.DocDate).Name("DOC_DATE")
+                .Member(m => m.SalesmanCode).Name("SALESMAN_CODE ")
+                .Member(m => m.CancelAutoGlProc).Name("CANCEL_AUTO_GL_PROC ")
+                .Member(m => m.EbookDocdate).Name("EBOOK_DOCDATE")
+                .Member(m => m.DefnFldsList).Name("DEFNFLDSLIST")
+                .Member(m => m.Transactions).Name("TRANSACTIONS")
+                ;
 
         }
     }
