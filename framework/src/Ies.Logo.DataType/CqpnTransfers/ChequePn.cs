ac7402ada57d44a5767cc00fe434c10b9bb027a6
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-20 17:01:37 +0300

    CqpnTransfers domaini eklendi

---
+++ framework/src/Ies.Logo.DataType/CqpnTransfers/ChequePn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ies.Logo.DataType.AggregateRoot;
+using Ies.Logo.DataType.Infrastructure;
+
+namespace Ies.Logo.DataType.CqpnTransfers
+{
+    [Serializable]
+    public class ChequePn : AuditedAggregateRoot, IAuxilCode
+    {
+        public static string XmlRoot => "CQPN_TRANSFERS";
+
+        public virtual Nullable<int> Type { get; set; }
+        public virtual Nullable<short> CurrentStatus{ get; set; }
+        public virtual string Number{ get; set; }
+        public virtual string Owing{ get; set; }
+        public virtual Nullable<DateTime> DueDate{ get; set; }
+        public virtual Nullable<decimal> Amount{ get; set; }
+        public virtual Nullable<decimal> TcXrate{ get; set; }
+        public virtual Nullable<decimal> TcAmount{ get; set; }
+        public virtual Nullable<short> Transferred{ get; set; }
+        public virtual string OwnaccCode{ get; set; }
+        public virtual string ArpCode{ get; set; }
+        public virtual string NewSerialNo{ get; set; }
+        public virtual Nullable<short> AffectRisk{ get; set; }
+        public virtual string BankTitle{ get; set; }
+        public virtual string City{ get; set; }
+        public virtual Nullable<short> Currency{ get; set; }
+        public virtual Nullable<short> CurrTrans{ get; set; }
+        public virtual string BankCode { get; set; }
+
+        #region Implementation
+        public virtual string AuxilCode { get; set; }
+        #endregion
+    }
+}
