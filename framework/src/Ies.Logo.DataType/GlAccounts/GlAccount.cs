d1e8885b455a047a29dcb842880b503a709ce464
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-21 11:14:14 +0300

    GlAccounts domaini eklendi

---
+++ framework/src/Ies.Logo.DataType/GlAccounts/GlAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Ies.Logo.DataType.AggregateRoot;
+using Ies.Logo.DataType.Infrastructure;
+
+namespace Ies.Logo.DataType.GlAccounts
+{
+    [Serializable]
+    public class GlAccount : AuditedAggregateRoot, IRecordStatus, ISpecialCode, IGroupCode, IDataSiteId
+    {
+        public static string XmlRoot => "GL_ACCOUNTS";
+
+        public virtual string Code { get; set; }
+        public virtual string Description{ get; set; }
+        public virtual string Description2{ get; set; }
+        public virtual string Unit{ get; set; }
+        public virtual string OhpCode{ get; set; }
+        public virtual string DiffaccCode{ get; set; }
+        public virtual Nullable<int> AccountType{ get; set; }
+        public virtual Nullable<int> MndtryQuan{ get; set; }
+        public virtual Nullable<int> MndtryOhp{ get; set; }
+        public virtual Nullable<int> AccountChar{ get; set; }
+        public virtual Nullable<int> InflationFlag{ get; set; }
+        public virtual string DiffdebtaccCode{ get; set; }
+        public virtual Nullable<int> Category{ get; set; }
+        public virtual Nullable<int> ProjectControl{ get; set; }
+        public virtual string InfDiffAcccode{ get; set; }
+        public virtual Nullable<int> Ccurency{ get; set; }
+        public virtual Nullable<int> Curratetype{ get; set; }
+        public virtual Nullable<int> Fixedcurrtype{ get; set; }
+        public virtual string ClName{ get; set; }
+        public virtual string TaxNr{ get; set; }
+        public virtual Nullable<int> FoxTaxDecl{ get; set; }
+        public virtual Nullable<int> VatAcc{ get; set; }
+        public virtual Nullable<int> IsBdgtLine{ get; set; }
+        public virtual string BdgtAccCode{ get; set; }
+        public virtual string BgdtReflAccCode{ get; set; }
+        public virtual string BdgtPayAccCode{ get; set; }
+        public virtual string BdgtPayReflAccCode{ get; set; }
+        public virtual Nullable<int> Crbdgtaccln{ get; set; }
+        public virtual Nullable<int> Crbdgtpayaln{ get; set; }
+        public virtual string CorpCode1 { get; set; }
+        public virtual string CorpCode2 { get; set; }
+        public virtual string CorpCode3 { get; set; }
+        public virtual string CorpCode4 { get; set; }
+        public virtual string FuncCode1 { get; set; }
+        public virtual string FuncCode2 { get; set; }
+        public virtual string FuncCode3 { get; set; }
+        public virtual string FuncCode4 { get; set; }
+        public virtual string FinCode { get; set; }
+        public virtual string EcoCode1 { get; set; }
+        public virtual string EcoCode2 { get; set; }
+        public virtual string EcoCode3 { get; set; }
+        public virtual string EcoCode4 { get; set; }
+        public virtual string VatReflAccCode{ get; set; }
+        public virtual string VatReflOthAccCode{ get; set; }
+
+        #region Implementation
+        public virtual Nullable<short> RecordStatus { get; set; }
+        public virtual string AuthCode { get; set; }
+        public virtual string AuxilCode { get; set; }
+        public virtual string GroupCode { get; set; }
+        public virtual Nullable<int> DataSiteId { get; set; }
+        #endregion
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/GlAccounts/GlAccountProfile.cs
@@ -0,0 +1,74 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.GlAccounts
+{
+    public class GlAccountProfile : IConfigurationProfile 
+    {
+        public static GlAccountProfile Default { get; } = new GlAccountProfile();
+
+        private GlAccountProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<GlAccount>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : GlAccount
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("GL_ACCOUNT");
+
+            typeConfigurations
+                .Member(m => m.Code).Name("CODE")
+                .Member(m => m.Description).Name("DESCRIPTION")
+                .Member(m => m.Description2).Name("DESCRIPTION2")
+                .Member(m => m.Unit).Name("UNIT")
+                .Member(m => m.OhpCode).Name("OHP_CODE")
+                .Member(m => m.DiffaccCode).Name("DIFFACC_CODE")
+                .Member(m => m.AccountType).Name("ACCOUNT_TYPE")
+                .Member(m => m.MndtryQuan).Name("MNDTRY_QUAN")
+                .Member(m => m.MndtryOhp).Name("MNDTRY_OHP")
+                .Member(m => m.AccountChar).Name("ACCOUNT_CHAR")
+                .Member(m => m.InflationFlag).Name("INFLATION_FLAG")
+                .Member(m => m.DiffdebtaccCode).Name("DIFFDEBTACC_CODE")
+                .Member(m => m.Category).Name("CATEGORY")
+                .Member(m => m.ProjectControl).Name("PROJECT_CONTROL")
+                .Member(m => m.InfDiffAcccode).Name("INF_DIFF_ACCCODE")
+                .Member(m => m.Ccurency).Name("CCURRENCY")
+                .Member(m => m.Curratetype).Name("CURRATETYPE")
+                .Member(m => m.Fixedcurrtype).Name("FIXEDCURRTYPE")
+                .Member(m => m.ClName).Name("CL_NAME")
+                .Member(m => m.TaxNr).Name("TAX_NR")
+                .Member(m => m.FoxTaxDecl).Name("FOR_TAX_DECL")
+                .Member(m => m.VatAcc).Name("VAT_ACC")
+                .Member(m => m.IsBdgtLine).Name("IS_BDGT_LINE")
+                .Member(m => m.BdgtAccCode).Name("BDGT_ACC_CODE")
+                .Member(m => m.BgdtReflAccCode).Name("BDGT_REFL_ACC_CODE")
+                .Member(m => m.BdgtPayAccCode).Name("BDGT_PAY_ACC_CODE")
+                .Member(m => m.BdgtPayReflAccCode).Name("BDGT_PAY_REFL_ACC_CODE")
+                .Member(m => m.Crbdgtaccln).Name("CRBDGTACCLN")
+                .Member(m => m.Crbdgtpayaln).Name("CRBDGTPAYALN")
+                .Member(m => m.CorpCode1).Name("CORP_CODE1")
+                .Member(m => m.CorpCode2).Name("CORP_CODE2")
+                .Member(m => m.CorpCode3).Name("CORP_CODE3")
+                .Member(m => m.CorpCode4).Name("CORP_CODE4")
+                .Member(m => m.FuncCode1).Name("FUNC_CODE1")
+                .Member(m => m.FuncCode2).Name("FUNC_CODE2")
+                .Member(m => m.FuncCode3).Name("FUNC_CODE3")
+                .Member(m => m.FuncCode4).Name("FUNC_CODE4")
+                .Member(m => m.FinCode).Name("FIN_CODE")
+                .Member(m => m.EcoCode1).Name("ECO_CODE1")
+                .Member(m => m.EcoCode2).Name("ECO_CODE2")
+                .Member(m => m.EcoCode3).Name("ECO_CODE3")
+                .Member(m => m.EcoCode4).Name("ECO_CODE4")
+                .Member(m => m.VatReflAccCode).Name("VAT_REFL_ACC_CODE")
+                .Member(m => m.VatReflOthAccCode).Name("VAT_REFL_OTH_ACC_CODE")
+                ;
+        }
+    }
+}
