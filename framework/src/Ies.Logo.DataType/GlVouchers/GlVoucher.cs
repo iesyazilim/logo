b336952c4b7c51a3787c1bca9ea6ccfe16a8414a
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-21 11:28:39 +0300

    GlVouchers domaini tanımlandı

---
+++ framework/src/Ies.Logo.DataType/GlVouchers/GlVoucher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Ies.Logo.DataType.AggregateRoot;
+
+namespace Ies.Logo.DataType.GlVouchers
+{
+    [Serializable]
+    public class GlVoucher : AuditedAggregateRoot
+    {
+        public static string XmlRoot => "GL_VOUCHERS";
+
+
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/GlVouchers/GlVoucherProfile.cs
@@ -0,0 +1,32 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.GlVouchers
+{
+    public class GlVoucherProfile
+    {
+        public static GlVoucherProfile Default { get; } = new GlVoucherProfile();
+
+        private GlVoucherProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<GlVoucher>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : GlVoucher
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("GL_VOUCHER");
+
+            typeConfigurations
+                .Member(m => m.).Name("")
+                ;
+        }
+    }
+}
