d5360cdabddad9c114653bc1cf64e487bda2dde1
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-24 17:10:10 +0300

    SdTransactions domaini oluşturuldu SdTransaction.Transaction modeli eklendi

---
+++ framework/src/Ies.Logo.DataType/SdTransactions/SdTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ies.Logo.DataType.AggregateRoot;
+
+namespace Ies.Logo.DataType.SdTransactions
+{
+    [Serializable]
+    public class SdTransaction : AuditedAggregateRoot
+    {
+        public static string XmlRoot => "SD_TRANSACTIONS";
+
+        #region Implementation
+
+        #endregion
+
+        #region SubClasses
+        public virtual Transaction AttachmentArp { get; set; }
+        #endregion
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/SdTransactions/SdTransactionProfile.cs
@@ -0,0 +1,32 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.SdTransactions
+{
+    public class SdTransactionProfile : IConfigurationProfile
+    {
+        public static SdTransactionProfile Default { get; } = new SdTransactionProfile();
+
+        private SdTransactionProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<SdTransaction>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : SdTransaction
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("SD_TRANSACTION");
+
+            typeConfigurations
+                .Member(m => m.AttachmentArp).Name("ATTACHMENT_ARP")
+                ;
+        }
+    }
+}
