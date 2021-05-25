dbc1985f3a347b4538a041df307505b79b599006
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-25 15:26:36 +0300

    Services domaini bitirildi

--- framework/src/Ies.Logo.DataType/Services/Service.cs
+++ framework/src/Ies.Logo.DataType/Services/Service.cs
@@ -1,17 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Ies.Logo.DataType.AggregateRoot;
 using Ies.Logo.DataType.GlLinks;
+using Ies.Logo.DataType.Infrastructure;
 using Ies.Logo.DataType.Units;
 using Ies.Logo.DataType.WhParams;
 
 namespace Ies.Logo.DataType.Services
 {
-    public class Service : AuditedAggregateRoot
+    public class Service : AuditedAggregateRoot, IRecordStatus, IDetailedAuxilCode, IAuthCode, IPaymentCode, IDataSiteId
     {
         public virtual ServiceCardType CardType { get; set; }
+        public virtual string Code { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Description2 { get; set; }
+        public virtual Nullable<decimal> VatPerc { get; set; }
+        public virtual string UnitsetCode { get; set; }
+        public virtual Nullable<decimal> Returnvat { get; set; }
+        public virtual Nullable<short> ImportExpenses { get; set; }
+        public virtual Nullable<decimal> AffectCost { get; set; }
+        public virtual string AddTaxcode { get; set; }
+        public virtual Nullable<short> DistType { get; set; }
+        public virtual Nullable<short> Candeduct { get; set; }
+        public virtual Nullable<int> DeductionPart1 { get; set; }
+        public virtual Nullable<int> DeductionPart2 { get; set; }
+        public virtual Nullable<int> ExtAccessFlags { get; set; }
+        public virtual Nullable<short> Currdif { get; set; }
 
         #region Implementation 
-
+        public virtual Nullable<short> RecordStatus { get; set; }
+        public virtual string AuxilCode { get; set; }
+        public virtual string AuxilCode2 { get; set; }
+        public virtual string AuxilCode3 { get; set; }
+        public virtual string AuxilCode4 { get; set; }
+        public virtual string AuxilCode5 { get; set; }
+        public virtual string AuthCode { get; set; }
+        public virtual string PaymentCode { get; set; }
+        public virtual Nullable<int> DataSiteId { get; set; }
         #endregion
 
         #region SubClasses
--- framework/src/Ies.Logo.DataType/Services/ServiceProfile.cs
+++ framework/src/Ies.Logo.DataType/Services/ServiceProfile.cs
@@ -1,6 +1,6 @@
 ﻿using ExtendedXmlSerializer;
 using ExtendedXmlSerializer.Configuration;
-using Ies.Logo.DataType.Xml; 
+using Ies.Logo.DataType.Xml;
 
 namespace Ies.Logo.DataType.Services
 {
@@ -26,6 +26,21 @@ namespace Ies.Logo.DataType.Services
             
             typeConfigurations
                 .Member(m => m.CardType).Name("CARD_TYPE")
+                .Member(m => m.Code).Name("CODE")
+                .Member(m => m.Description).Name("DESCRIPTION")
+                .Member(m => m.Description2).Name("DESCRIPTION2")
+                .Member(m => m.VatPerc).Name("VAT_PERC")
+                .Member(m => m.UnitsetCode).Name("UNITSET_CODE")
+                .Member(m => m.Returnvat).Name("RETURNVAT")
+                .Member(m => m.ImportExpenses).Name("IMPORT_EXPENSES")
+                .Member(m => m.AffectCost).Name("AFFECT_COST")
+                .Member(m => m.AddTaxcode).Name("ADD_TAXCODE")
+                .Member(m => m.DistType).Name("DIST_TYPE")
+                .Member(m => m.Candeduct).Name("CANDEDUCT")
+                .Member(m => m.DeductionPart1).Name("DEDUCTION_PART1")
+                .Member(m => m.DeductionPart2).Name("DEDUCTION_PART2")
+                .Member(m => m.ExtAccessFlags).Name("EXT_ACCESS_FLAGS")
+                .Member(m => m.Currdif).Name("CURRDIFF")
                 .Member(m => m.WhParams).Name("WH_PARAMS")
                 .Member(m => m.Units).Name("UNITS")
                 .Member(m => m.GlLinks).Name("GL_LINKS")
