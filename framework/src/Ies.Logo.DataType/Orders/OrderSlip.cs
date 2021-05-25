b2aca003e8f16032ec987e50419a9ae0cd7c9cd6
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-25 17:11:36 +0300

    Orders domaini oluşturuldu

---
+++ framework/src/Ies.Logo.DataType/Orders/OrderSlip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Ies.Logo.DataType.AggregateRoot;
+
+namespace Ies.Logo.DataType.Orders
+{
+    [Serializable]
+    public class OrderSlip : AuditedAggregateRoot
+    {
+        public virtual bool IsSales { get; set; }
+
+        #region Implementation
+
+        #endregion
+
+        #region SubClasses
+
+        #endregion
+
+        public override string GetRootElementName()
+        {
+            switch (IsSales)
+            {
+                case false:
+                    return "PURCHASE_ORDERS";
+
+                case true:
+                    return "SALES_ORDERS";
+
+                default:
+                    return "UNKOWN";
+            }
+        }
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/Orders/OrderSlipProfile.cs
@@ -0,0 +1,32 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.Orders
+{
+    public class OrderSlipProfile : IConfigurationProfile
+    {
+        public static OrderSlipProfile Default { get; } = new OrderSlipProfile();
+
+        private OrderSlipProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<OrderSlip>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : OrderSlip
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("ORDER_SLIP");
+
+            typeConfigurations
+                .Member(m => m.IsSales).Ignore()
+                ;
+        }
+    }
+}
