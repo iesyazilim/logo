4eb5eb42a6b1cb58d691a97377178e108de06ff5
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-05-27 12:01:03 +0300

    SerialLotRecords domaini eklendi

---
+++ framework/src/Ies.Logo.DataType/SerialLotRecords/ItemSerialLot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ies.Logo.DataType.AggregateRoot;
+using Ies.Logo.DataType.Infrastructure;
+
+namespace Ies.Logo.DataType.SerialLotRecords
+{
+    [Serializable]
+    public class ItemSerialLot : AuditedAggregateRoot, IDataSiteId
+    {
+        public static string XmlRoot => "SERIAL_LOT_RECORDS";
+
+        public virtual string ItemCode { get; set; }
+        public virtual Nullable<int> Type { get; set; }
+        public virtual string Code { get; set; }
+        public virtual string Description { get; set; }
+        public virtual Nullable<int> WfStatus { get; set; }
+        public virtual string VariantCode { get; set; }
+
+        #region Implementation
+        public virtual Nullable<int> DataSiteId { get; set; }
+        #endregion
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/SerialLotRecords/ItemSerialLotProfile.cs
@@ -0,0 +1,37 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.SerialLotRecords
+{
+    public class ItemSerialLotProfile : IConfigurationProfile
+    {
+        public static ItemSerialLotProfile Default { get; } = new ItemSerialLotProfile();
+
+        private ItemSerialLotProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<ItemSerialLot>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : ItemSerialLot
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("ITEM_SERIAL_LOT");
+
+            typeConfigurations
+                .Member(m => m.ItemCode).Name("ITEM_CODE")
+                .Member(m => m.Type).Name("TYPE")
+                .Member(m => m.Code).Name("CODE")
+                .Member(m => m.Description).Name("DESCRIPTION")
+                .Member(m => m.WfStatus).Name("WF_STATUS")
+                .Member(m => m.VariantCode).Name("VARIANT_CODE")
+                ;
+        }
+    }
+}
