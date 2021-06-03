0354aa57522a61c3283e59ff7ba1081a207f58be
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-03 09:38:28 +0300

    LogoClaims eklendi

---
+++ framework/src/Ies.Logo.Core/Security/LogoClaimTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace Ies.Logo.Core.Security
+{
+    public static class LogoClaimTypes
+    {
+        public const string FirmNumber = "logo_firm_number";
+
+        public const string FirmPeriod = "logo_firm_period";
+
+        public const string EndpointAddress = "logo_endpoint_address";
+
+        public const string UserNumber = "logo_user_number";
+    }
+}
---
+++ framework/src/Ies.Logo.Core/Security/LogoClaimsIdentityExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Ies.Logo.Core.Security;
+
+namespace System.Security.Principal
+{
+    public static class LogoClaimsIdentityExtensions
+    {
+        public static string FindFirmNumber(this ClaimsPrincipal principal)
+        {
+            var firmNumber = principal.Claims?.FirstOrDefault(c => c.Type == LogoClaimTypes.FirmNumber);
+            if (firmNumber == null || string.IsNullOrEmpty(firmNumber.Value))
+            {
+                return null;
+            }
+
+            return firmNumber.Value;
+        }
+
+        public static string FindFirmPeriod(this ClaimsPrincipal principal)
+        {
+            var firmPeriod = principal.Claims?.FirstOrDefault(c => c.Type == LogoClaimTypes.FirmPeriod);
+            if (firmPeriod == null || string.IsNullOrEmpty(firmPeriod.Value))
+            {
+                return null;
+            }
+
+            return firmPeriod.Value;
+        }
+
+        public static string FindEndpointAddress(this ClaimsPrincipal principal)
+        {
+            var endpointAddress = principal.Claims?.FirstOrDefault(c => c.Type == LogoClaimTypes.EndpointAddress);
+            if (endpointAddress == null || string.IsNullOrEmpty(endpointAddress.Value))
+            {
+                return null;
+            }
+
+            return endpointAddress.Value;
+        }
+
+        public static short? FindUserNumber(this ClaimsPrincipal principal)
+        {
+            var userNumber = principal.Claims?.FirstOrDefault(c => c.Type == LogoClaimTypes.UserNumber);
+
+            if (userNumber == null || string.IsNullOrEmpty(userNumber.Value))
+            {
+                return null;
+            }
+
+            if (short.TryParse(userNumber.Value, out short result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+    }
+}
