32624ec40e4744e9b9c6e456e22909e059829e7a
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-04 10:04:46 +0300

    Manager projesi eklendi

---
+++ framework/src/Ies.Logo.Manager/LogoManager.cs
@@ -0,0 +1,14 @@
+﻿using Ies.Logo.Core.Configuration;
+
+namespace Ies.Logo.Manager
+{
+    public class LogoManager : ILogoService
+    {
+        protected ILogoConnectionConfiguration Configuration { get; }
+
+        public LogoManager(ILogoConnectionConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+    }
+}
