e721e6ec2cb6c816e9ee433dbf33345eb1543c25
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-07 09:58:52 +0300

    GetListAsync metodu eklendi

---
+++ samples/Ies.SampleApp/src/Ies.SampleApp.DataAccess/Items/ItemDal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Ies.Logo.Core.Configuration;
+
+namespace Ies.SampleApp.Items
+{
+    public class ItemDal : IItemDal
+    {
+        protected ILogoConnectionConfiguration Configuration { get; }
+
+        public ItemDal(ILogoConnectionConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public async Task<List<GetItemListDto>> GetListAsync()
+        {
+            using (var connection = Configuration.Create())
+            {
+                string query = $@"
+                WITH Item AS(
+                SELECT LOGICALREF DataReference, CODE Code, NAME Name, ACTIVE Active FROM  LG_{Configuration.FirmNumber}_ITEMS
+                ) 
+                SELECT * FROM Item 
+                ";
+
+                return (await connection.QueryAsync<GetItemListDto>(query)).ToList();
+            }
+        }
+    }
+}
