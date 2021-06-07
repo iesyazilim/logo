89cec1c3a17872f21d32b1b1b0e470517eea487d
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-07 10:50:38 +0300

    ConsoleUI projesi eklendi

---
+++ samples/Ies.SampleApp/src/Ies.SampleApp.ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Ies.SampleApp.DepedencyResolvers.Microsoft;
+using Ies.SampleApp.Items;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ies.SampleApp.ConsoleUI
+{
+    class Program
+    {
+        static ServiceProvider resolver { get; } = CreateDI();
+
+        static async Task Main(string[] args)
+        {
+            await ItemJobs();
+        }
+
+        static async Task ItemJobs()
+        {
+            var itemService = resolver.GetService<IItemService>();
+            var itemList = await itemService.GetListAsync();
+        }
+
+        static ServiceProvider CreateDI()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+
+            serviceCollection.Resolve();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
