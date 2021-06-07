1950ce28e2d8b1507bac36d9f97c099702e40573
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-07 13:22:06 +0300

    ItemController Crud işlemleri eklendi

--- samples/Ies.SampleApp/src/Ies.SampleApp.HttpApi/Controllers/ItemController.cs
+++ samples/Ies.SampleApp/src/Ies.SampleApp.HttpApi/Controllers/ItemController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Ies.Logo.DataType.Items;
 using Ies.SampleApp.Items;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,7 +18,19 @@ namespace Ies.SampleApp.HttpApi.Controllers
         }
 
         [HttpGet]
+        [Route("get-by-id/{id=}")]
+        public async Task<Item> GetById(int id) => await _itemService.GetAsync(id);
+
+        [HttpGet]
         [Route("list")]
         public async Task<List<GetItemListDto>> GetListAsync() => await _itemService.GetListAsync();
+
+        [HttpPost]
+        [Route("add-or-update")]
+        public async Task<int> AddOrUpdate(Item item) => await _itemService.AddOrUpdateAsync(item);
+
+        [HttpDelete]
+        [Route("{id=}")]
+        public async Task Delete(int id) => await _itemService.DeleteAsync(id);
     }
 }
