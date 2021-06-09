f785d90d562ff006e2c4fcf97f10c6c749b43d20
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-09 10:42:34 +0300

    Business projes dosya yolu güncellendi

---
+++ samples/Ies.MultiFirm/src/Ies.MultiFirm.Business/ClCards/IClCardService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ies.Logo.DataType.ArAps;
+using Ies.Logo.Manager;
+
+namespace Ies.MultiFirm.ClCards
+{
+    public interface IClCardService : ILogoCrudService<ArAp>
+    {
+        Task<List<GetClCardListDto>> GetListAsync();
+    }
+}
