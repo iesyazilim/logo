186c734863ab54ba1dac0e5fbc9c052fbceb96c6
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-09 15:15:34 +0300

    FirmController eklendi

---
+++ samples/Ies.MultiFirm/src/Ies.MultiFirm.HttpApi/Controllers/FirmController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ies.MultiFirm.HttpApi.Controllers
+{
+    [Route("api/firm")]
+    [ApiController]
+    public class FirmController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get-principal")]
+        public IActionResult GetPrincipal()
+        {
+            return Ok(new
+            {
+                FirmNumber = Request.Cookies["firm-number"],
+                FirmPeriod = Request.Cookies["firm-period"],
+                EndpointAddress = Request.Cookies["logo-endpoint-address"],
+            });
+        }
+
+        [HttpPost]
+        [Route("set-principal")]
+        public IActionResult SetPrincipal(PrincipalInfo principalInfo)
+        {
+            Response.Cookies.Append("firm-number", principalInfo.FirmNumber);
+            Response.Cookies.Append("firm-period", principalInfo.FirmPeriod);
+            Response.Cookies.Append("logo-endpoint-address", principalInfo.EndpointAddress);
+            return Ok();
+        }
+    }
+    public class PrincipalInfo
+    {
+       public string FirmNumber { get; set; }
+       public string FirmPeriod { get; set; }
+       public string EndpointAddress { get; set; }
+    }
+}
