9e6f6c24ba4a99e50fa05a28ba9607a7fb861a9f
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-09 10:26:31 +0300

    GetclCardListDto eklendi

---
+++ samples/Ies.MultiFirm/src/Ies.MultiFirm.Entities/ClCards/GetClCardListDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Ies.MultiFirm.ClCards
+{
+    public class GetClCardListDto : IDto
+    {
+        public int DataReference { get; set; }
+        public string Code { get; set; }
+        public string Definition { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Active { get; set; }
+    }
+}
