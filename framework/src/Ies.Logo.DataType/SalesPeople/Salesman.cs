a843f24c66e0ec322a9e22845c046f7f9db36ddf
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-14 17:15:44 +0300

    SalesPeople domaini oluşturuldu

---
+++ framework/src/Ies.Logo.DataType/SalesPeople/Salesman.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Ies.Logo.Core;
+using Ies.Logo.DataType.AggregateRoot;
+using Ies.Logo.DataType.Infrastructure;
+
+namespace Ies.Logo.DataType.SalesPeople
+{
+    public class Salesman : AuditedAggregateRoot, IDataNo, IAuxilCode, IAuthCode, IRecordStatus
+    {
+        public string DataNumber { get => Code; set => Code = value; }
+        public virtual string Code { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Position { get; set; }
+        public virtual int FirmNo { get; set; }
+        public virtual int Targets { get; set; }
+        public virtual Nullable<int> Type { get; set; }
+
+        #region Implementation
+        public virtual string AuxilCode { get; set; }
+        public virtual string AuthCode { get; set; }
+        public virtual Nullable<short> RecordStatus { get; set; }
+        #endregion
+
+        #region SubClasses
+
+        #endregion
+
+        public override LogoObjectType GetRootElementName()
+        {
+            switch (Type)
+            {
+                case 0:
+                default:
+                    return LogoObjectType.NOXMLCLASS_15;
+
+                case 1:
+                    return LogoObjectType.NOXMLCLASS_65;
+            }
+        }
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/SalesPeople/SalesmanProfile.cs
@@ -0,0 +1,36 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.SalesPeople
+{
+    public class SalesmanProfile : IConfigurationProfile
+    {
+        public static SalesmanProfile Default { get; } = new SalesmanProfile();
+
+        private SalesmanProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<Salesman>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : Salesman
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("SALESMAN");
+
+            typeConfigurations
+                .Member(m => m.Code).Name("CODE")
+                .Member(m => m.Name).Name("NAME")
+                .Member(m => m.Position).Name("POSITION")
+                .Member(m => m.FirmNo).Name("FIRM_NO")
+                .Member(m => m.Targets).Name("TARGETS")
+                ;
+        }
+    }
+}
