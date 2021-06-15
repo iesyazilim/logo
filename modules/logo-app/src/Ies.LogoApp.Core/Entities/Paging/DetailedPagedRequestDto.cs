efb4d988208dea475d50746966cc81d9bf42ce07
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-15 14:07:56 +0300

    DetailedPagedReqeustDto eklendi

---
+++ modules/logo-app/src/Ies.LogoApp.Core/Entities/Paging/DetailedPagedRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Allegory.Standart.Filter.Concrete;
+
+namespace Ies.LogoApp.Core.Entities
+{
+    [Serializable]
+    public class DetailedPagedRequestDto : PagedRequestDto
+    {
+        public string OrderBy { get; set; }
+
+        public Condition Conditions { get; set; }
+    }
+}
