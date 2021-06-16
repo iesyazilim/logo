f930242f0158edbcb64eebb183570e4d1da1116e
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-16 15:13:16 +0300

    Ies.LogoApp.ConsoleUI projesi eklendi

---
+++ modules/logo-app/src/Ies.LogoApp.ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ies.LogoApp.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ modules/logo-app/src/Ies.LogoApp.Resolver.Microsoft/Resolver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using Ies.Logo.Core.Configuration;
+using Ies.Logo.Repositories;
+using Ies.Logo.ServiceAdapter;
+using Ies.LogoApp.Items;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ies.LogoApp.Resolver.Microsoft
+{
+    public static class Resolver
+    {
+        public static void ResolveLogoApp(this IServiceCollection serviceProvider, ServiceLifetime serviceLifetime, params LogoConnectionConfiguration[] configurations)
+        {
+            if(configurations == null)
+            {
+                serviceProvider.AddSingleton(o =>
+                {
+                    var configuration = o.GetService<IConfiguration>();
+                    var firms = new List<LogoConnectionConfiguration>();
+                    configuration.Bind("Firms", firms);
+                    return firms;
+                });
+            }
+            else
+            {
+                serviceProvider.AddSingleton(o =>
+                {
+                    return configurations.ToList();
+                });
+            }
+
+            serviceProvider.LogoResolve(serviceLifetime);
+            serviceProvider.ItemsResolve(serviceLifetime);
+        }
+
+        static void LogoResolve(this IServiceCollection serviceProvider, ServiceLifetime serviceLifetime)
+        {
+            serviceProvider.Add(new ServiceDescriptor(
+                typeof(ILogoConnectionConfiguration),
+                o => 
+                {
+                    var firm = o.GetService<List<LogoConnectionConfiguration>>()
+                             .FirstOrDefault(f => f.FirmNumber == Thread.CurrentPrincipal.FindFirmNumber()
+                                                  && f.FirmPeriod == Thread.CurrentPrincipal.FindFirmPeriod()
+                                                  && f.EndpointAddress == Thread.CurrentPrincipal.FindEndpointAddress());
+
+                    if (firm == null)
+                        throw new Exception("Firm not found");
+
+                    return firm;
+                },
+                serviceLifetime));
+
+            serviceProvider.Add(new ServiceDescriptor(typeof(ILogoObjectServiceConfiguration), o => o.GetService<ILogoConnectionConfiguration>(), serviceLifetime));
+            serviceProvider.Add(new ServiceDescriptor(typeof(ILogoConfiguration), o => o.GetService<ILogoConnectionConfiguration>(), serviceLifetime));
+            serviceProvider.Add(new ServiceDescriptor(typeof(ILogoObjectService), typeof(LogoObjectServiceAdapter), serviceLifetime));
+            serviceProvider.Add(new ServiceDescriptor(typeof(ILogoRepository<>), typeof(LogoRepository<>), serviceLifetime));
+        }
+        static void ItemsResolve(this IServiceCollection serviceProvider, ServiceLifetime serviceLifetime)
+        {
+            serviceProvider.Add(new ServiceDescriptor(typeof(IItemDal), typeof(ItemDal), serviceLifetime));
+            serviceProvider.Add(new ServiceDescriptor(typeof(IItemService), typeof(ItemManager), serviceLifetime));
+        }
+    }
+}
