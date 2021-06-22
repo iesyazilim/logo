46403304747394d3766ce2c8fa7508512b73d858
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-22 13:14:25 +0300

    Item QueryBase sorgusu güncellendi

--- modules/logo-app/src/Ies.LogoApp.DataAccess/Items/ItemDal.cs
+++ modules/logo-app/src/Ies.LogoApp.DataAccess/Items/ItemDal.cs
@@ -10,13 +10,26 @@ namespace Ies.LogoApp.Items
             QueryBase = @"WITH List AS
             (
                 SELECT 
-                 LOGICALREF Id
-                ,CODE Code
-                ,NAME Name
-                ,NAME3 Name2
-                ,NAME4 Name3
-                ,ACTIVE Active
+
+                ITEMS.LOGICALREF Id
+                ,ITEMS.CARDTYPE CardType
+                ,ITEMS.CODE Code
+                ,ITEMS.NAME Name
+                ,ITEMS.NAME3 Name2
+                ,ITEMS.NAME4 Name3
+                ,UNITL.CODE MainUnitCode
+                ,ITEMS.SPECODE SpecialCode
+                ,ITEMS.STGRPCODE GroupCode
+                ,ITEMS.PRODUCERCODE ProducerCode
+                ,ISNULL(GNTOTST.ONHAND,0) OnhandStock
+                ,ISNULL(GNTOTST.RESERVED,0) ReservedStock
+                ,ITEMS.ACTIVE Active
+
                 FROM LG_{firm}_ITEMS 
+                LEFT JOIN LG_{firm}_UNITSETF UNITF ON UNITF.LOGICALREF=ITEMS.UNITSETREF
+                LEFT JOIN LG_{firm}_UNITSETL UNITL ON UNITF.LOGICALREF=UNITL.UNITSETREF AND UNITL.MAINUNIT=1
+                LEFT JOIN LV_{firm}_{period}_GNTOTST GNTOTST ON  ITEMS.LOGICALREF=GNTOTST.STOCKREF AND INVENNO=-1 
+
                 WHERE CARDTYPE<>22
             )";
         }
--- modules/logo-app/src/Ies.LogoApp.Entities/Items/GetItemListDto.cs
+++ modules/logo-app/src/Ies.LogoApp.Entities/Items/GetItemListDto.cs
@@ -5,10 +5,17 @@ namespace Ies.LogoApp.Items
     public class GetItemListDto : IDto
     {
         public int Id { get; set; }
+        public short CardType { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Name2 { get; set; }
         public string Name3 { get; set; }
+        public string MainUnitCode { get; set; }
+        public string SpecialCode { get; set; }
+        public string GroupCode { get; set; }
+        public string ProducerCode { get; set; }
+        public decimal OnhandStock { get; set; }
+        public decimal ReservedStock { get; set; }
         public bool Active { get; set; }
     }
 }
