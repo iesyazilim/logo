b00a18cb8c4b2ec031a2e6b6405baeec4b4861c8
ahmetfarukulu <ahmetfarukulu@gmail.com>
2021-06-22 09:58:28 +0300

    LogoExtension eklendi

---
+++ framework/src/Ies.Logo.Core/Extensions/LogoExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ies.Logo.Core.Extensions
+{
+    public static class LogoExtension
+    {
+        public static DateTime ToLogoTime(this int time)
+        {
+            int HH, MM, SS;
+            HH = (time - (time % 65536)) / 65536 / 256;
+            MM = ((time - (time % 65536)) / 65536 - ((time - (time % 65536)) / 65536 / 256) * 256);
+            SS = (((time % 65536) - ((time % 65536) % 256)) / 256);
+            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, HH, MM, SS);
+        }
+
+        public static int ToLogoTime(this DateTime time) => (time.Hour * 65536 * 256) + (time.Minute * 65536) + (time.Second * 256);
+    }
+}
