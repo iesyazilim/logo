3a0101c529670a798f0c24b4dadd799b38ecbaab
Emre Düver <dver.emre@gmail.com>
2022-08-10 17:05:45 +0300

    Malzeme satın alma için fiyat kartı ekleme eklendi.

---
+++ framework/src/Ies.Logo.DataType/PriceCards/Divisions/DivisionStrProfile.cs
@@ -0,0 +1,34 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Ies.Logo.DataType.PriceCards.Divisions
+{
+    public class DivisionStrProfile : IConfigurationProfile
+    {
+        public static DivisionStrProfile Default { get; } = new DivisionStrProfile();
+
+        private DivisionStrProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<DivisionStr>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : DivisionStr
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("DIVISION_STR");
+
+            typeConfigurations.Member(m => m.DivisionCodes).Name("DIVCODES");
+        }
+    }
+}
