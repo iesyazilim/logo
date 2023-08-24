154ea828e9e9632a220d313064455f7716f8a345
Emre Düver <dver.emre@gmail.com>
2023-08-24 11:35:59 +0300

    IesGateway içine async olmayan metodlar eklendi. Execute eklendi.

--- framework/src/Ies.Logo.Core/Configuration/ILogoConnectionConfiguration.cs
+++ framework/src/Ies.Logo.Core/Configuration/ILogoConnectionConfiguration.cs
@@ -8,13 +8,22 @@ namespace Ies.Logo.Core.Configuration
     {
         SqlConnection Create();
 
+        T QueryFirst<T>(string query, string parameters = null);
+        T QueryFirstOrDefault<T>(string query, string parameters = null);
+        List<T> QueryList<T>(string query, string parameters = null);
         Task<T> QueryFirstAsync<T>(string query, string parameters = null);
+        Task<T> QueryFirstOrDefaultAsync<T>(string query, string parameters = null);
         Task<List<T>> QueryListAsync<T>(string query, string parameters = null);
 
+        T QueryScalar<T>(string query, string parameters = null);
+        List<T> QueryListScalar<T>(string query, string parameters = null);
         Task<T> QueryScalarAsync<T>(string query, string parameters = null);
         Task<List<T>> QueryListScalarAsync<T>(string query, string parameters = null);
 
+        PagedQueryResponse<T> QueryPagedRequest<T>(string query, string parameters = null);
         Task<PagedQueryResponse<T>> QueryPagedRequestAsync<T>(string query, string parameters = null);
-        Task<List<string>> GetQueryMultipleAsync(string query, string parameters = null);
+
+        int Execute(string query, string parameters = null);
+        Task<int> ExecuteAsync(string query, string parameters = null);
     }
 }
--- framework/src/Ies.Logo.Core/Configuration/LogoConnectionConfiguration.cs
+++ framework/src/Ies.Logo.Core/Configuration/LogoConnectionConfiguration.cs
@@ -27,21 +27,59 @@ namespace Ies.Logo.Core.Configuration
             return connection;
         }
 
+        #region Query
+
+        public T QueryFirst<T>(string query, string parameters = null)
+        {
+            var result = GetQuery(query, parameters);
+            return JsonConvert.DeserializeObject<List<T>>(result).First();
+        }
+        public T QueryFirstOrDefault<T>(string query, string parameters = null)
+        {
+            var result = GetQuery(query, parameters);
+            return JsonConvert.DeserializeObject<List<T>>(result).FirstOrDefault();
+        }
+        public List<T> QueryList<T>(string query, string parameters = null)
+        {
+            var result = GetQuery(query, parameters);
+            return JsonConvert.DeserializeObject<List<T>>(result);
+        }
+
         public async Task<T> QueryFirstAsync<T>(string query, string parameters = null)
+        {
+            var result = await GetQueryAsync(query, parameters);
+            return JsonConvert.DeserializeObject<List<T>>(result).First();
+        }
+        public async Task<T> QueryFirstOrDefaultAsync<T>(string query, string parameters = null)
         {
             var result = await GetQueryAsync(query, parameters);
             return JsonConvert.DeserializeObject<List<T>>(result).FirstOrDefault();
         }
-
         public async Task<List<T>> QueryListAsync<T>(string query, string parameters = null)
         {
             var result = await GetQueryAsync(query, parameters);
             return JsonConvert.DeserializeObject<List<T>>(result);
         }
 
+        #endregion
+
+        #region QueryScalar
         public async Task<T> QueryScalarAsync<T>(string query, string parameters = null)
         {
             var result = await GetQueryAsync(query, parameters);
+
+            var reponse = JsonConvert.DeserializeObject<List<JObject>>(result).ToList();
+
+            if (reponse.Count <= 0)
+            {
+                return default(T);
+            }
+
+            return reponse.FirstOrDefault().Properties().FirstOrDefault().Value.ToObject<T>();
+        }
+        public T QueryScalar<T>(string query, string parameters = null)
+        {
+            var result = GetQuery(query, parameters);
 
             var reponse = JsonConvert.DeserializeObject<List<JObject>>(result).ToList();
 
@@ -68,18 +106,25 @@ namespace Ies.Logo.Core.Configuration
 
             return list;
         }
-
-        public async Task<List<string>> GetQueryMultipleAsync(string query, string parameters = null)
+        public List<T> QueryListScalar<T>(string query, string parameters = null)
         {
-            var client = CreateClient();
+            var result = GetQuery(query, parameters);
 
-            var result = await client.GetMultipleQueryAsync(query, parameters);
+            var list = new List<T>();
 
-            if (!result.IsSuccess)
-                throw new Exception(result.Error);
+            var reponse = JsonConvert.DeserializeObject<List<JObject>>(result).ToList();
 
-            return result.Value;
+            foreach (var item in reponse)
+            {
+                list.Add(item.Properties().FirstOrDefault().Value.ToObject<T>());
+            }
+
+            return list;
         }
+
+        #endregion
+
+        #region QueryPaged
 
         public async Task<PagedQueryResponse<T>> QueryPagedRequestAsync<T>(string query, string parameters = null)
         {
@@ -91,7 +136,31 @@ namespace Ies.Logo.Core.Configuration
                 TotalCount = (int)(JArray.Parse(results.LastOrDefault())).FirstOrDefault()["TotalCount"]
             };
         }
+        public PagedQueryResponse<T> QueryPagedRequest<T>(string query, string parameters = null)
+        {
+            var results = GetQueryMultiple(query, parameters);
+            var rows = JsonConvert.DeserializeObject<List<T>>(results.FirstOrDefault());
+            return new PagedQueryResponse<T>()
+            {
+                Rows = rows,
+                TotalCount = (int)(JArray.Parse(results.LastOrDefault())).FirstOrDefault()["TotalCount"]
+            };
+        }
 
+        #endregion
+
+        #region Execute
+        public int Execute(string query, string parameters = null)
+        {
+            return RunExecute(query, parameters);
+        }
+        public async Task<int> ExecuteAsync(string query, string parameters = null)
+        {
+            return await RunExecuteAsync(query, parameters);
+        }
+        #endregion
+
+        #region GatewayRequest
         private GeneralSvcClient CreateClient()
         {
             GeneralSvcClient client = new GeneralSvcClient();
@@ -115,5 +184,66 @@ namespace Ies.Logo.Core.Configuration
 
             return result.Value;
         }
+
+        private string GetQuery(string query, string parameters = null)
+        {
+            var client = CreateClient();
+
+            var result = client.GetQuery(query, parameters);
+
+            if (!result.IsSuccess)
+                throw new Exception(result.Error);
+
+            return result.Value;
+        }
+
+        private async Task<List<string>> GetQueryMultipleAsync(string query, string parameters = null)
+        {
+            var client = CreateClient();
+
+            var result = await client.GetMultipleQueryAsync(query, parameters);
+
+            if (!result.IsSuccess)
+                throw new Exception(result.Error);
+
+            return result.Value;
+        }
+
+        private List<string> GetQueryMultiple(string query, string parameters = null)
+        {
+            var client = CreateClient();
+
+            var result = client.GetMultipleQuery(query, parameters);
+
+            if (!result.IsSuccess)
+                throw new Exception(result.Error);
+
+            return result.Value;
+        }
+
+        private async Task<int> RunExecuteAsync(string query, string parameters = null)
+        {
+            var client = CreateClient();
+
+            var result = await client.ExecuteAsync(query, parameters);
+
+            if (!result.IsSuccess)
+                throw new Exception(result.Error);
+
+            return result.Value;
+        }
+
+        private int RunExecute(string query, string parameters = null)
+        {
+            var client = CreateClient();
+
+            var result = client.Execute(query, parameters);
+
+            if (!result.IsSuccess)
+                throw new Exception(result.Error);
+
+            return result.Value;
+        }
+        #endregion
     }
 }
