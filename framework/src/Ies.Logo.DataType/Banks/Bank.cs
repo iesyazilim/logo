71fbc503972b398c4ce9081b9f0daecae7f9a4ad
Emre Düver <78685922+EmreDuver@users.noreply.github.com>
2024-05-08 18:07:15 +0300

    Bank eklendi

---
+++ framework/src/Ies.Logo.DataType/Banks/Bank.cs
@@ -0,0 +1,49 @@
+﻿using Ies.Logo.Core;
+using Ies.Logo.DataType.AggregateRoot;
+using Ies.Logo.DataType.DefinitionFields;
+using Ies.Logo.DataType.GrpCodes;
+using Ies.Logo.DataType.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ies.Logo.DataType.Banks
+{
+    [Serializable]
+    public class Bank : AuditedAggregateRoot, IRecordStatus, IAuxilCode, IAuthCode, IDataSiteId
+    {
+        public static LogoObjectType XmlRoot => LogoObjectType.BANKS;
+
+        public virtual string Code { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Division { get; set; }
+        public virtual string DivisionId { get; set; }
+        public virtual string Address1 { get; set; }
+        public virtual string Address2 { get; set; }
+        public virtual string District { get; set; }
+        public virtual string Town { get; set; }
+        public virtual string City { get; set; }
+        public virtual string CountryCode { get; set; }
+        public virtual string Country { get; set; }
+        public virtual string PostalCode { get; set; }
+        public virtual string Telephone1 { get; set; }
+        public virtual string Telephone2 { get; set; }
+        public virtual string Fax { get; set; }
+        public virtual string Contact { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string WebUrl { get; set; }
+        public virtual string CorrpAcc { get; set; }
+        public virtual string Voen { get; set; }
+
+        #region Implementation
+        public virtual Nullable<short> RecordStatus { get; set; }
+        public virtual string AuxilCode { get; set; }
+        public virtual string AuthCode { get; set; }
+        public virtual Nullable<int> DataSiteId { get; set; }
+        #endregion
+
+        #region SubClasses
+        public virtual List<DefnFld> DefnFldsList { get; set; }
+        #endregion
+    }
+}
---
+++ framework/src/Ies.Logo.DataType/Banks/BankProfile.cs
@@ -0,0 +1,53 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ies.Logo.DataType.Banks
+{
+    public class BankProfile : IConfigurationProfile
+    {
+        public static BankProfile Default { get; } = new BankProfile();
+
+        private BankProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<Bank>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : Bank
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("BANK");
+
+            typeConfigurations
+                .Member(m => m.Code).Name("CODE")
+                .Member(m => m.Title).Name("TITLE")
+                .Member(m => m.Division).Name("DIVISION")
+                .Member(m => m.DivisionId).Name("DIVISION_ID")
+                .Member(m => m.Address1).Name("ADDRESS1")
+                .Member(m => m.Address2).Name("ADDRESS2")
+                .Member(m => m.District).Name("DISTRICT")
+                .Member(m => m.Town).Name("TOWN")
+                .Member(m => m.City).Name("CITY")
+                .Member(m => m.Country).Name("COUNTRY")
+                .Member(m => m.CountryCode).Name("COUNTRY_CODE")
+                .Member(m => m.PostalCode).Name("POSTAL_CODE")
+                .Member(m => m.Telephone1).Name("TELEPHONE1")
+                .Member(m => m.Telephone2).Name("TELEPHONE2")
+                .Member(m => m.Fax).Name("FAX")
+                .Member(m => m.Contact).Name("CONTACT")
+                .Member(m => m.Email).Name("E_MAIL")
+                .Member(m => m.WebUrl).Name("WEB_URL")
+                .Member(m => m.CorrpAcc).Name("CORRP_ACC")
+                .Member(m => m.Voen).Name("VOEN");
+        }
+    }
+}
