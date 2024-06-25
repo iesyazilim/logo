30a6847ca168690ccc9bf99ceb4876d5c45ee6e9
Emre Düver <78685922+EmreDuver@users.noreply.github.com>
2024-06-25 18:21:21 +0300

    OhpAccount eklendi

---
+++ framework/src/Ies.Logo.DataType/OhpAccounts/OhpAccountProfile.cs
@@ -0,0 +1,35 @@
+﻿using ExtendedXmlSerializer;
+using ExtendedXmlSerializer.Configuration;
+using Ies.Logo.DataType.Xml;
+
+namespace Ies.Logo.DataType.OhpAccounts
+{
+    public class OhpAccountProfile : IConfigurationProfile
+    {
+        public static OhpAccountProfile Default { get; } = new OhpAccountProfile();
+
+        private OhpAccountProfile() { }
+
+        public IConfigurationContainer Get(IConfigurationContainer parameter)
+        {
+            var configuration = parameter.Type<OhpAccount>();
+
+            Configure(configuration);
+
+            return configuration;
+        }
+
+
+        public static void Configure<T>(ITypeConfiguration<T> typeConfigurations) where T : OhpAccount
+        {
+            typeConfigurations.Configure();
+            typeConfigurations.Name<T>("OHP_ACCOUNT");
+
+            typeConfigurations
+                 .Member(m => m.Code).Name("CODE")
+                 .Member(m => m.Description).Name("DESCRIPTION")
+                 .Member(m => m.Unit).Name("UNIT")
+                 .Member(m => m.DataSiteId).Name("DATA_SITEID");
+        }
+    }
+}
