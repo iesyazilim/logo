be61d0fb5f29d36d2c0c576a6f790e8bae6d2ba5
murat kemerci <muratkemerci@outlook.com.tr>
2025-08-28 17:13:53 +0300

    cari fişleri satır seviyesi "masraf etkiler" eklendi

--- framework/src/Ies.Logo.DataType/ArpVouchers/Transactions/Transaction.cs
+++ framework/src/Ies.Logo.DataType/ArpVouchers/Transactions/Transaction.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Ies.Logo.DataType.Auditing;
+﻿using Ies.Logo.DataType.Auditing;
 using Ies.Logo.DataType.DefinitionFields;
 using Ies.Logo.DataType.Infrastructure;
 using Ies.Logo.DataType.Payments;
+using System;
+using System.Collections.Generic;
 
 namespace Ies.Logo.DataType.ArpVouchers
 {
@@ -14,47 +14,48 @@ namespace Ies.Logo.DataType.ArpVouchers
         public virtual string Tranno { get; set; }
         public virtual string DocNumber { get; set; }
         public virtual string Description { get; set; }
-        public virtual Nullable<decimal> Debit{ get; set; }
+        public virtual Nullable<decimal> Debit { get; set; }
         public virtual Nullable<decimal> Credit { get; set; }
-        public virtual Nullable<short> CurrTrans{ get; set; }
-        public virtual Nullable<decimal> TcXrate{ get; set; }
-        public virtual Nullable<decimal> TcAmount{ get; set; }
-        public virtual Nullable<decimal> RcXrate{ get; set; }
-        public virtual Nullable<decimal> RcAmount{ get; set; }
-        public virtual Nullable<int> Extenref{ get; set; }
-        public virtual Nullable<int> CurrselTrans{ get; set; }
-        public virtual Nullable<short> SinglePayment{ get; set; }
-        public virtual Nullable<short> Discounted{ get; set; }
-        public virtual Nullable<decimal> DiscountRate{ get; set; }
-        public virtual Nullable<decimal> VatRate{ get; set; }
-        public virtual Nullable<decimal> DiscountedAmount{ get; set; }
-        public virtual Nullable<decimal> InflationIdx{ get; set; }
-        public virtual string CashTranGrpNo{ get; set; }
-        public virtual Nullable<int> CashTranGrplineNo{ get; set; }
-        public virtual Nullable<decimal> CashInfldx{ get; set; }
-        public virtual Nullable<int> CashOrgLogoId{ get; set; }
-        public virtual Nullable<int> InvoiceOrgLogoId{ get; set; }
-        public virtual string CreditCardNo{ get; set; }
-        public virtual string EximFilecode{ get; set; }
-        public virtual Nullable<int> EximFilelinenr{ get; set; }
-        public virtual Nullable<int> EximProcnr{ get; set; }
-        public virtual Nullable<decimal> Fundsharerat{ get; set; }
-        public virtual Nullable<int> Month{ get; set; }
-        public virtual Nullable<int> Year{ get; set; }
-        public virtual Nullable<short> AffectCollatrl{ get; set; }
-        public virtual Nullable<short> Grpfirmtrans{ get; set; }
-        public virtual Nullable<short> AffectRisk{ get; set; }
-        public virtual string BatchNr{ get; set; }
+        public virtual Nullable<short> CurrTrans { get; set; }
+        public virtual Nullable<decimal> TcXrate { get; set; }
+        public virtual Nullable<decimal> TcAmount { get; set; }
+        public virtual Nullable<decimal> RcXrate { get; set; }
+        public virtual Nullable<decimal> RcAmount { get; set; }
+        public virtual Nullable<int> Extenref { get; set; }
+        public virtual Nullable<int> CurrselTrans { get; set; }
+        public virtual Nullable<short> SinglePayment { get; set; }
+        public virtual Nullable<short> Discounted { get; set; }
+        public virtual Nullable<decimal> DiscountRate { get; set; }
+        public virtual Nullable<decimal> VatRate { get; set; }
+        public virtual Nullable<decimal> DiscountedAmount { get; set; }
+        public virtual Nullable<decimal> InflationIdx { get; set; }
+        public virtual string CashTranGrpNo { get; set; }
+        public virtual Nullable<int> CashTranGrplineNo { get; set; }
+        public virtual Nullable<decimal> CashInfldx { get; set; }
+        public virtual Nullable<int> CashOrgLogoId { get; set; }
+        public virtual Nullable<int> InvoiceOrgLogoId { get; set; }
+        public virtual string CreditCardNo { get; set; }
+        public virtual string EximFilecode { get; set; }
+        public virtual Nullable<int> EximFilelinenr { get; set; }
+        public virtual Nullable<int> EximProcnr { get; set; }
+        public virtual Nullable<decimal> Fundsharerat { get; set; }
+        public virtual Nullable<int> Month { get; set; }
+        public virtual Nullable<int> Year { get; set; }
+        public virtual Nullable<short> AffectCollatrl { get; set; }
+        public virtual Nullable<short> Grpfirmtrans { get; set; }
+        public virtual Nullable<short> AffectRisk { get; set; }
+        public virtual Nullable<short> AffectCost { get; set; }
+        public virtual string BatchNr { get; set; }
         public virtual string ApproveNr { get; set; }
         public virtual string OrgLogoId { get; set; }
         public virtual string BankAccCode { get; set; }
         public virtual string BankGlCode { get; set; }
-        public virtual string BankOhpCode{ get; set; }
-        public virtual Nullable<DateTime> DevirProcDate{ get; set; }
-        public virtual Nullable<DateTime> DocDate{ get; set; }
-        public virtual string SalesmanCode{ get; set; }
-        public virtual string PointcommGlCode{ get; set; }
-        public virtual Nullable<decimal> VatAmount{ get; set; }
+        public virtual string BankOhpCode { get; set; }
+        public virtual Nullable<DateTime> DevirProcDate { get; set; }
+        public virtual Nullable<DateTime> DocDate { get; set; }
+        public virtual string SalesmanCode { get; set; }
+        public virtual string PointcommGlCode { get; set; }
+        public virtual Nullable<decimal> VatAmount { get; set; }
         public virtual string RetCcFcNo { get; set; }
 
         #region Implementation
