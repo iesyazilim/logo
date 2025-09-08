ae9f291fbcbd8580fa1fb17f55196ab2b09e37e3
Emre Düver <78685922+EmreDuver@users.noreply.github.com>
2025-09-08 16:32:15 +0300

    BankVoucherWithInvoice eklendi

---
+++ framework/src/Ies.Logo.DataType/BankVouchers/Invoice/BankVoucherWithInvoice.cs
@@ -0,0 +1,16 @@
+﻿using Ies.Logo.Core;
+using Ies.Logo.DataType.Invoices;
+using System;
+using System.Collections.Generic;
+
+namespace Ies.Logo.DataType.BankVouchers
+{
+    [Serializable]
+    public class BankVoucherWithInvoice : BankVoucher
+    {
+        public static new LogoObjectType XmlRoot => BankVoucher.XmlRoot;
+
+        public virtual string BnAccCode { get; set; }
+        public List<Invoice> AttachmentInvoices { get; set; }
+    }
+}
--- framework/src/Ies.Logo.DataType/Xml/SerializerExtension.cs
+++ framework/src/Ies.Logo.DataType/Xml/SerializerExtension.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using ExtendedXmlSerializer;
+using Ies.Logo.DataType.BankVouchers;
+using Ies.Logo.DataType.Infrastructure;
+using Ies.Logo.DataType.ItemCharacteristics;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
-using ExtendedXmlSerializer;
-using Ies.Logo.DataType.Infrastructure;
-using Ies.Logo.DataType.ItemCharacteristics;
 
 namespace Ies.Logo.DataType.Xml
 {
@@ -25,16 +26,10 @@ namespace Ies.Logo.DataType.Xml
                                    .Insert(0, "<?xml version=\"1.0\" encoding=\"ISO-8859-9\"?>\n");
             }
 
-
-            /*
-             * CHARACTERISTIC_CODES xml'i içindeki VALUES tagının child'ı da VALUES olduğu için hata alıyoruz.
-             * Child VALUE'lar VALUES olarak düzeltilip addorupdate isteği atılıyor
-             */
-            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(CharacteristicCode))
-                return Serializer.Serialize(new List<T> { data }).Replace("<VALUE>", "<VALUES>").Replace("</VALUE>", "</VALUES>");
-
-            return Serializer.Serialize(new List<T> { data });
+            var xml = Serializer.Serialize(new List<T> { data });
+            return XmlStringHandlingSerialize<T>(xml);
         }
+
         public static string Serialize<T>(this List<T> data, bool autoSet = true) where T : ILogoBase
         {
             if (autoSet)
@@ -47,16 +42,10 @@ namespace Ies.Logo.DataType.Xml
                 return doc.ToString().Insert(0, "<?xml version=\"1.0\" encoding=\"ISO-8859-9\"?>\n");
             }
 
-
-            /*
-             * CHARACTERISTIC_CODES xml'i içindeki VALUES tagının child'ı da VALUES olduğu için hata alıyoruz.
-             * Child VALUE'lar VALUES olarak düzeltilip addorupdate isteği atılıyor
-             */
-            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(CharacteristicCode))
-                return Serializer.Serialize(data).Replace("<VALUE>", "<VALUES>").Replace("</VALUE>", "</VALUES>");
-
-            return Serializer.Serialize(data);
+            var xml = Serializer.Serialize(data);
+            return XmlStringHandlingSerialize<T>(xml);
         }
+
         public static T Deserialize<T>(this string xml) where T : ILogoBase
         {
             xml = xml.Replace("ISO-8859-9", "utf-8");
@@ -67,20 +56,11 @@ namespace Ies.Logo.DataType.Xml
                 return Serializer.Get<T>().Deserialize<T>(doc.Root.FirstNode.ToString());
             }
 
-            /*
-             * CHARACTERISTIC_CODES xml'i içindeki VALUES tagının child'ı da VALUES olduğu için hata alıyoruz.
-             * Child VALUES'lar VALUE olarak düzeltilip get isteği atılıyor
-             */
-            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(CharacteristicCode))
-            {
-                var startIndex = xml.IndexOf("<VALUES>") + "<VALUES>".Length;
-                var closedIndex = xml.LastIndexOf("</VALUES>");
-                var str = xml.Substring(startIndex, closedIndex - startIndex).Replace("<VALUES>", "<VALUE>").Replace("</VALUES>", "</VALUE>");
-                xml = string.Concat(xml.Substring(0, startIndex), str, xml.Substring(closedIndex));
-            }
+            xml = XmlStringHandlingDeserialize<T>(xml);
 
             return Serializer.Get<T>().Deserialize<List<T>>(xml).FirstOrDefault();
         }
+
         public static List<T> DeserializeList<T>(this string xml) where T : ILogoBase
         {
             xml = xml.Replace("ISO-8859-9", "utf-8");
@@ -93,6 +73,57 @@ namespace Ies.Logo.DataType.Xml
             }
 
             return Serializer.Get<T>().Deserialize<List<T>>(xml);
+        }
+
+        /// <summary>
+        /// CHARACTERISTIC_CODES xml'i içindeki VALUES tagının child'ı da VALUES olduğu için hata alıyoruz. Child VALUES'lar VALUE olarak düzeltilip get isteği atılıyor
+        /// BankVoucherWithInvoice: BankVoucher içindeki TRANSACTION tagı ile Invoice içindeki TRANSACTION tagı karıştığı için,INVOICE_TRANSACTION olarak düzeltilip get isteği atılıyor
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        private static string XmlStringHandlingDeserialize<T>(string xml)
+        {
+            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(CharacteristicCode))
+            {
+                var startIndex = xml.IndexOf("<VALUES>") + "<VALUES>".Length;
+                var closedIndex = xml.LastIndexOf("</VALUES>");
+                var str = xml.Substring(startIndex, closedIndex - startIndex).Replace("<VALUES>", "<VALUE>").Replace("</VALUES>", "</VALUE>");
+                xml = string.Concat(xml.Substring(0, startIndex), str, xml.Substring(closedIndex));
+            }
+
+            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(BankVoucherWithInvoice))
+            {
+                var openValue = "<ATTACHMENT_INVOICE>";
+                var closeValue = "</ATTACHMENT_INVOICE>";
+                var startIndex = xml.IndexOf(openValue) + openValue.Length;
+                var closedIndex = xml.LastIndexOf(closeValue);
+                var str = xml.Substring(startIndex, closedIndex - startIndex);
+                str = str.Replace("<TRANSACTION>", "<INVOICE_TRANSACTION>").Replace("</TRANSACTION>", "</INVOICE_TRANSACTION>");
+
+                xml = string.Concat(xml.Substring(0, startIndex), str, xml.Substring(closedIndex));
+            }
+
+            return xml;
+        }
+
+        /// <summary>
+        /// CHARACTERISTIC_CODES xml'i içindeki VALUES tagının child'ı da VALUES olduğu için hata alıyoruz. Child VALUE'lar VALUES olarak düzeltilip addorupdate isteği atılıyor
+        /// BankVoucherWithInvoice: BankVoucher içindeki TRANSACTION tagı ile Invoice içindeki TRANSACTION tagı karıştığı için,INVOICE_TRANSACTION olarak düzeltilip addorupdate isteği atılıyor
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="autoSet"></param>
+        /// <returns></returns>
+        private static string XmlStringHandlingSerialize<T>(string xml)
+        {
+            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(CharacteristicCode))
+                xml = xml.Replace("<VALUE>", "<VALUES>").Replace("</VALUE>", "</VALUES>");
+
+            if (typeof(T).GetProperty("XmlRoot").DeclaringType.Name == nameof(BankVoucherWithInvoice))
+                xml = xml.Replace("<INVOICE_TRANSACTION>", "<TRANSACTION>").Replace("</INVOICE_TRANSACTION>", "</TRANSACTION>");
+
+            return xml;
         }
     }
 }
